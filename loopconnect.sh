82233fe02ea8478f85d4f4f726c1553f6d48736f
GiDRaPiTaRdA <chriss@ASUS-EEDFROJL>
2022-04-14 21:24:49 +0300

    source

---
+++ loopconnect.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+YELLOW='\033[0;33m'
+NC='\033[0m' # No Color
+
+while getopts s:p:t: flag
+do
+    case "${flag}" in
+        s) server=${OPTARG};;
+        p) port=${OPTARG};;
+	t) turbo=${OPTARG};;
+    esac
+done
+
+echo -e "${YELLOW}Start loop connect"
+
+echo "Server: $server";
+echo "Port: $port";
+echo -e "Turbo: $turbo${NC}";
+
+
+while :
+do
+  sudo proxychains python3 DRipper.py -t $turbo -p $port -s $server
+done
+
