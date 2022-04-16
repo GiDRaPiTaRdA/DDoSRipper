94f473524c36c08147c74c5d11f3efcad956ec71
GiDRaPiTaRdA <chriss@ASUS-EEDFROJL>
2022-04-16 22:51:36 +0300

    proxy

--- automation/proxiescheck.sh
+++ automation/proxiescheck.sh
@@ -14,7 +14,7 @@ fi
 
 while :
 do
-    C:/Users/Maxim/Documents/Source/web/ProxySocket/TestConcole/bin/Debug/TestConcole.exe ip socks5 10 C:/Users/Maxim/Documents/Source/web/prox/good/socks5.csv true
+    C:/Users/Maxim/Documents/Source/web/ProxySocket/TestConcole/bin/Debug/TestConcole.exe ip socks5 400 C:/Users/Maxim/Documents/Source/web/prox/good/socks5.csv true
     ./publish.sh
     echo "Wait $delay"
     sleep $delay
