2f8844f7282e7355970c997ca4c58f15266f373c
GiDRaPiTaRdA <xxmaxdrivezz@gmail.com>
2022-04-16 22:48:40 +0300

    proxy

---
+++ automation/proxiescheck.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+
+while getopts t: flag
+do
+    case "${flag}" in
+        t) delay=${OPTARG};;
+    esac
+done
+
+if [ -z "$delay" ]; then
+    delay="15m"
+fi
+
+while :
+do
+    C:/Users/Maxim/Documents/Source/web/ProxySocket/TestConcole/bin/Debug/TestConcole.exe ip socks5 10 C:/Users/Maxim/Documents/Source/web/prox/good/socks5.csv true
+    ./publish.sh
+    echo "Wait $delay"
+    sleep $delay
+done
