e4abdbdea1fc8a13e53cbb06505c046017e522f9
GiDRaPiTaRdA <xxmaxdrivezz@gmail.com>
2022-04-22 02:14:56 +0300

    proxy

---
+++ automation/kill.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+#ps -axjf -a
+
+#sudo pkill -9 -f -c "launcher.sh"
+
+
+function killprocess(){
+pname=$1
+
+id=$(pgrep -f $pname)
+
+if [ ! -z "$id" ]
+	then
+	echo "Kill $pname"
+        echo $id
+	kill -9 $id
+	#sudo pkill -TERM -P $id
+fi
+
+#processes=$(ps aux | grep "$pname")
+#echo "$processes"
+
+#ids=$(echo $processes | awk '{print $2}')
+
+#echo $ids
+#sudo kill ${ids}
+
+}
+
+killprocess "launcher.sh"
+killprocess "run.sh"
+killprocess "startauto.sh"
+killprocess "loopconnect.sh"
+killprocess "DRipper.py"
+
+# ps aux -h process view hierarchy
+# pkill -TERM -P 27888
+# sudo kill -9 17070
+# pgrep -f "launcher.sh"
+# sudo kill -9 $(pgrep -f "launcher.sh")C
