71028c9f787bbd17fe0ed146de6fdd2f29486b19
root <root@ASUS-EEDFROJL>
2022-05-04 16:39:18 +0300

    proxy

---
+++ automation/proxiescheckcmd.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+YELLOW='\033[0;33m'
+NC='\033[0m' # No Color
+
+while getopts t: flag
+do
+    case "${flag}" in
+        t) delay=${OPTARG};;
+    esac
+done
+
+if [ -z "$delay" ]; then
+    delay="15m"
+fi
+
+while :
+do
+    /mnt/c/Users/Maxim/Documents/Source/web/ProxySocket/TestConcole/bin/Debug/TestConcole.exe ip socks5 400 /mnt/c/Users/Maxim/Documents/Source/web/prox/good/socks5.csv true false
+    ./automation/publish.sh
+    echo -e "${YELLOW}Wait $delay${NC}"
+    sleep $delay
+done
