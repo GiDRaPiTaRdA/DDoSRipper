01b51ad9e9e4f9414dd59a3c1970606260992ea9
GiDRaPiTaRdA <xxmaxdrivezz@gmail.com>
2022-05-07 16:24:16 +0300

    proxy

--- automation/shell/proxiescheckcmd.sh
+++ automation/shell/proxiescheckcmd.sh
@@ -3,6 +3,9 @@
 YELLOW='\033[0;33m'
 NC='\033[0m' # No Color
 
+get=5
+
+
 while getopts t: flag
 do
     case "${flag}" in
@@ -16,7 +19,7 @@ fi
 
 while :
 do
-    /mnt/c/Users/Maxim/Documents/Source/web/ProxySocket/TestConcole/bin/Debug/TestConcole.exe ip socks5 1000 C:/Users/Maxim/Documents/Source/web/prox/good/socks5.csv true false
+    /mnt/c/Users/Maxim/Documents/Source/web/ProxySocket/TestConcole/bin/Debug/TestConcole.exe ip socks5 $get C:/Users/Maxim/Documents/Source/web/prox/good/socks5.csv true false
     ./automation/publish.sh
     echo -e "${YELLOW}Wait $delay${NC}"
     sleep $delay
