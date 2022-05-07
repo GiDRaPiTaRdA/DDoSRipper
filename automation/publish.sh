a099785f400a2d0f136bd1a52be91ad1c0898419
GiDRaPiTaRdA <xxmaxdrivezz@gmail.com>
2022-05-07 19:29:29 +0300

    proxy

--- automation/publish.sh
+++ automation/publish.sh
@@ -4,8 +4,8 @@ cd automation/proxy/data
 sha1sum proxychains.conf > proxychains.conf.sha1
 cd ../../
 
-#git fetch
-#git add .
-#git status
-#git commit -m "proxy"
-#git push origin master
+git fetch
+git add .
+git status
+git commit -m "proxy"
+git push origin master
--- automation/updateproxies.sh
+++ automation/updateproxies.sh
@@ -4,5 +4,5 @@ YELLOW='\033[0;33m'
 NC='\033[0m' # No Color
 repo=https://raw.githubusercontent.com/GiDRaPiTaRdA/DDoSRipper/master
 echo -e "${YELLOW}Fetch proxychains.conf${NC}"
-sudo wget ${repo}automation/proxy/proxychains.conf -O /etc/proxychains.conf
+sudo wget ${repo}/automation/proxy/proxychains.conf -O /etc/proxychains.conf
 
