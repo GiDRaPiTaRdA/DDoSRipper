f7fdf9c19aa16a1c3e38765c3ce5023db99201a2
GiDRaPiTaRdA <xxmaxdrivezz@gmail.com>
2022-05-17 12:48:21 +0300

    proxy

--- automation/updateproxies.sh
+++ automation/updateproxies.sh
@@ -4,5 +4,6 @@ YELLOW='\033[0;33m'
 NC='\033[0m' # No Color
 repo=https://raw.githubusercontent.com/GiDRaPiTaRdA/DDoSRipper/master
 echo -e "${YELLOW}Fetch proxychains.conf${NC}"
-sudo wget ${repo}/automation/proxy/data/proxychains.conf -O /etc/proxychains.conf
+sudo wget ${repo}/automation/proxy/data/proxychains.conf -O /tmp/proxychains.conf
+sudo cp /tmp/proxychains.conf /etc/proxychains.conf
 
