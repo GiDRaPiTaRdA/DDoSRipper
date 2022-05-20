30d2ae8235c6072835f0c0941bda04a308860ffc
root <root@ASUS-EEDFROJL>
2022-05-21 02:43:48 +0300

    proxy

--- automation/proxy/checkproxy.sh
+++ automation/proxy/checkproxy.sh
@@ -2,7 +2,22 @@
 
 proxy=$1
 timeout=20
+url1=http://icanhazip.com
+url=https://www.google.com/
+retry=4
 
-response=$(curl -s -m $timeout --socks5 $proxy http://icanhazip.com)
 
-echo $response
+for i in {1..4}
+do
+   # curl -I -s -m 10 --socks5 5.161.100.145:1080 http://icanhazip.com | head -n 1
+   response=$(curl -I -s -m $timeout --socks5 $proxy $url | head -n 1)
+
+   if [ ! -z "$response" ]
+   then
+       echo "Connected" 
+       break      	   #Abandon the loop.
+   fi
+
+   echo "retry $i"
+
+done
