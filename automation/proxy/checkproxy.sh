e6581ff7b0f6188ca9e11d27578e1e958703796c
root <root@ASUS-EEDFROJL>
2022-05-21 16:55:37 +0300

    proxy

--- automation/proxy/checkproxy.sh
+++ automation/proxy/checkproxy.sh
@@ -11,11 +11,11 @@ YELLOWL='\033[1;33m'
 BLUEL='\033[1;34m'
 GREENL='\033[0;32m'
 
-proxy=$1
+#proxy=$1
 timeout=20
 url1=http://icanhazip.com
 url=https://www.google.com/
-retry=4
+retry=2
 
 sourcefile="automation/proxy/data/socks5.csv"
 targetfile="automation/proxy/data/parcedproxies.txt"
@@ -24,27 +24,39 @@ ipregex='[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}:[0-9]{2,5}'
 
 echo -e "${BLUEL}Parse proxies...${NC}"
 proxies=($(grep -Eo $ipregex $sourcefile))
-echo -e "${GREENL}Found ${#proxies[@]} entries in $sourcefile ${NC}"
+echo -e "${GREENL}Found ${#proxies[@]} entries${NC} $sourcefile"
 
+proxychekedfile=/tmp/proxychecked.txt
+touch $proxychekedfile
+echo "" > $proxychekedfile
 
-## now loop through the above array
-for i in "${proxies[@]}"
+function check(){
+    proxy=$1
+    for ((i = 1 ; i <= $retry ; i++));
+    do
+       # curl -I -s -m 10 --socks5 5.161.100.145:1080 http://icanhazip.com | head -n 1
+       response=$(curl -I -s -m $timeout --socks5 $proxy $url | head -n 1)
+
+       if [ ! -z "$response" ]
+       then
+           echo "Connected $(echo $response | awk '{print $2}') $proxy"
+           echo $proxy >> $proxychekedfile
+           break
+       fi
+
+       #echo "retry $i"
+    done
+}
+
+echo "Start check proxies on $url"
+# now loop through the above array
+for proxy in "${proxies[@]}"
 do
-   echo "$i"
-   # or do whatever with individual element of the array
+     check $proxy &
 done
+
+wait
+
 exit 0
 
-for i in {1..$retry}
-do
-   # curl -I -s -m 10 --socks5 5.161.100.145:1080 http://icanhazip.com | head -n 1
-   response=$(curl -I -s -m $timeout --socks5 $proxy $url | head -n 1)
-
-   if [ ! -z "$response" ]
-   then
-       echo "Connected" 
-       break      	   #Abandon the loop.
-   fi
-
-   echo "retry $i"
-done
+echo "Verified ${#proxiesverified[@]}"
