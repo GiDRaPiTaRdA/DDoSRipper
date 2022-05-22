4465ba877396f3b5990863cfaf426ccca3881e51
root <root@ASUS-EEDFROJL>
2022-05-22 21:20:21 +0300

    proxy

--- automation/updateproxies.sh
+++ automation/updateproxies.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
 YELLOW='\033[0;33m'
+RED='\033[0;31m'
 NC='\033[0m' # No Color
+
 repo=https://raw.githubusercontent.com/GiDRaPiTaRdA/DDoSRipper/master
+repoproxy=${repo}/automation/proxy/data/proxychains.conf
+tmp=/tmp/proxychains.conf
+proxy=/etc/proxychains.conf
+
 echo -e "${YELLOW}Fetch proxychains.conf${NC}"
-sudo wget ${repo}/automation/proxy/data/proxychains.conf -O /tmp/proxychains.conf
-sudo cp /tmp/proxychains.conf /etc/proxychains.conf
+sudo wget $repoproxy -O $tmp
+
+if [ -e x.txt ]
+then
+    sudo cp $tmp $proxy
+    sudo rm $tmp
+else
+    echo -e "${RED}${tmp} does not exist${NC}"
+fi
 
